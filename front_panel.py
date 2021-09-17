bf5d8d684a2ca0a5cb2e6007e185de9efd7fda5c
mkpaulkim <mkkim.hcube@gmail.com>
2021-09-17 17:33:59 -0400

    working on front panel

--- front_panel.py
+++ front_panel.py
@@ -3,7 +3,7 @@ import tkinter as tk
 font0 = 'Consolas 10'
 
 
-def tkwindow(title, window=(20, 20, 500, 300), color='gray'):
+def tkwindow(title, window=(20, 20, 500, 300), color='gray80'):
     tkw = tk.Tk()
     tkw.title(title)
     x0, y0, ax, ay = window
@@ -12,8 +12,11 @@ def tkwindow(title, window=(20, 20, 500, 300), color='gray'):
     return tkw
 
 
+tkw = tkwindow(title='aaa')
+
+
 class CmdButton:
-    def __init__(self, frame, comm, text='push', xyw=(0, 0, 3), colors=('gray', 'yellow'), toggle=False):
+    def __init__(self, frame=tkw, xyw=(100, 10, 3), text='btn', colors=('gray70', 'yellow'), toggle=False):
         x, y, w = xyw
         off_color, on_color = colors
         self.button = tk.Button(frame, text=text, bg=off_color, width=w, font=font0)
@@ -21,14 +24,10 @@ class CmdButton:
         self.on_color = on_color
         self.off_color = off_color
         if toggle:
-            # self.command(self.switch)
-            self.button.config(command=comm)
+            self.command(self.switch)
 
     def command(self, command):
         self.button.config(command=command)
-
-    def color(self, color):
-        self.button['bg'] = color
 
     def on(self):
         self.button['bg'] = self.on_color
@@ -45,9 +44,70 @@ class CmdButton:
         else:
             self.on()
 
+    def color(self, color):
+        self.button['bg'] = color
+
+
+class ParamEntry:
+    def __init__(self, frame=tkw, xyw=(100, 50, 5), val=0, label='entry'):
+        x, y, w = xyw
+        self.entry = tk.Entry(frame, font=font0, justify=tk.RIGHT)
+        self.entry['width'] = w
+        self.entry.place(x=x, y=y)
+        self.set_entry(val)
+        self.lbl = tk.Label(frame, text=label, bg=frame['background'])
+        self.lbl.place(x=x, y=y)
+        tkw.update_idletasks()
+        lw = self.lbl.winfo_width()
+        self.lbl.place(x=x-lw-2, y=y+3)
+
+    def set_entry(self, val):
+        self.entry.delete(0, tk.END)
+        self.entry.insert(0, val)
+        self.entry.xview_moveto(1.)
+
+    def get_val(self, typ=int):
+        try:
+            val = typ(self.entry.get())
+        except ValueError:
+            if typ == str:
+                val = ''
+            else:
+                val = 0
+        return val
+
+
+class ProgressBar:
+    def __init__(self, frame=tkw, xyw=(100, 100, 100), label='progress'):
+        import tkinter.ttk as ttk
+        x, y, w = xyw
+        self.prog = ttk.Progressbar(frame)
+        self.prog.place(x=x, y=y)
+        self.lbl = tk.Label(frame, text=label, bg=frame['background'])
+        self.lbl.place(x=x, y=y)
+        tkw.update_idletasks()
+        lw = self.lbl.winfo_width()
+        self.lbl.place(x=x-lw-2, y=y)
+
+
+class SlideBar():
+    def __init__(self, frame=tkw, xyw=(100, 150, 100), label='slider'):
+        x, y, w = xyw
+        self.slider = tk.Scale(frame, orient=tk.HORIZONTAL, length=w, font=font0, bg=frame['background'])
+        self.slider.place(x=x, y=y)
+        self.lbl = tk.Label(frame, text=label, bg=frame['background'])
+        self.lbl.place(x=x, y=y)
+        tkw.update_idletasks()
+        lw = self.lbl.winfo_width()
+        self.lbl.place(x=x-lw-2, y=y+20)
+
 
 if __name__ == '__main__':
-    tkw = tkwindow(title='aaa')
-    btn1 = CmdButton(tkw, toggle=True)
+
+    btn1 = CmdButton()
+    ent1 = ParamEntry()
+    prog = ProgressBar()
+    sld = SlideBar()
 
     tk.mainloop()
+
